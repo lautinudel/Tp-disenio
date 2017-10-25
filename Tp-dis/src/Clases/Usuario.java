55ae2cdb859fefba387e057de5db7d975ac067ef
Unknown <lautaronudel@gmail.com>
2017-10-24 22:37:41 -0300

    Interfaces y clases

---
+++ Tp-dis/src/Clases/Usuario.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author L. Nudel
+ */
+public class Usuario {
+    private String nombre;
+    private String apellido;
+    private String username;
+    private Clave pass;
+    private ArrayList<Clave> historialPass;
+}
